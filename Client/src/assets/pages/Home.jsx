19a7c7c32f32a9c67969d51343ab43f7a0aaf5a5
Skiel221 <ezepelo1406@gmail.com>
2025-04-22 20:38:12 -0300

    avances en la dashboard.
    aaiiiaaai

--- Client/src/assets/pages/Home.jsx
+++ Client/src/assets/pages/Home.jsx
@@ -2,7 +2,16 @@ import './styles/home.css';
 import playBtn from '../images/home-images/play-boton.png';
 import antena from '../images/home-images/antena.png';
 import botonImg from '../images/home-images/botonera-img.png';
+import radioImg from '../images/home-images/radio.png';
+
 const Home = () => {
+    // Datos dinámicos
+    const emisoraEstadoDatos = [
+        { titulo: 'Programas Activos:', valor: 5 },
+        { titulo: 'Usuarios:', valor: 6 },
+        { titulo: 'Sonidos Totales:', valor: 32 }
+    ];
+
     return (
         <>
         <div className="contenido-home">
@@ -10,48 +19,58 @@ const Home = () => {
                 <div className='dashboard'>
                     <div className="titulos">
                         <div className="titulo">
-                            <img src={antena}/>
+                            <img src={antena} alt="Antena" />
                             <h2>Tus Programas</h2>
                         </div>
-                        <h4>Acceso rapido para tus programas</h4>
+                        <h4>Acceso rápido para tus programas</h4>
                     </div>
                     <div className="celda-dashboard">
                         <h3>contenido</h3>
                     </div>
                     <div className="celda-dashboard">
-                            <h3>contenido</h3>
+                        <h3>contenido</h3>
                     </div>
                     <div className="celda-dashboard">
                         <h3>contenido</h3>
                     </div>
                     <div className="celda-dashboard">
-                            <h3>contenido</h3>
+                        <h3>contenido</h3>
                     </div>
                 </div>
                 <div className='dashboard-botonera'>
                     <div className="titulo">
-                    <img src={botonImg}/>
-                    <h2>Tu Botonera</h2>
+                        <img src={botonImg} alt="Botonera" />
+                        <h2>Tu Botonera</h2>
                     </div>
-                    <h4>reproduce tus sonidos favoritos</h4>
+                    <h4>Reproduce tus sonidos favoritos</h4>
                     <div className="botones">
-                    <div className='boton'>
-                        <div className="boton-input">
-                            <img src={playBtn}/>
+                        <div className='boton'>
+                            <div className="boton-input">
+                                <img src={playBtn} alt="Play" />
+                            </div>
+                            <h5>Intro</h5>
                         </div>
-                        <h5>Intro</h5>
-                    </div>                 
-                    <div className="boton-añadir">
-                        <div className="boton-input-añadir">
-                            <h3>+</h3>
+                    </div>
+                </div>
+            </div>
+            <div className="emisora-estado">
+                <div className="titulo">
+                    <img src={radioImg} alt="Radio" />
+                    <h2>Estado de la emisora</h2>
+                </div>
+                <h4>Información general del estado de la emisora</h4>
+                <div className="emisora-estado-contenido">
+                    {emisoraEstadoDatos.map((dato, index) => (
+                        <div className="emisora-estado-celda" key={index}>
+                            <h3>{dato.titulo}</h3>
+                            <h3>{dato.valor}</h3>
                         </div>
-                        <h4>Añadir Boton</h4>
-                    </div>
-                    </div>
+                    ))}
                 </div>
             </div>
         </div>
         </>
-    )
-}
+    );
+};
+
 export default Home;
